c1d9e23a7072f3a3bfeba8aea3a42d009eaf6d2d
geekstartup42 <985343961@qq.com>
2019-02-27 01:14:51 +0800

    Update Median of Two Sorted Arrays.php

--- Median of Two Sorted Arrays.php
+++ Median of Two Sorted Arrays.php
@@ -1,1 +1,21 @@
-nice
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Float
+     */
+    function findMedianSortedArrays($nums1, $nums2) {
+        
+       if (empty($nums1) && empty($nums2)) {
+    		return (float)0;
+    	}
+        $nums = array_merge($nums1, $nums2);
+        sort($nums);
+        if (count($nums) % 2 !=0) {
+        	return $nums[count($nums) / 2 ];
+        }else{
+        	return  ($nums[count($nums) / 2 ]+ $nums[count($nums) / 2 -1]) / 2;
+        }
+    }
+}
