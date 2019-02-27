ddb3b31136c8a5776ae145155038382d9c0159c2
geekstartup42 <985343961@qq.com>
2019-02-27 16:44:23 +0800

    Update LongestPalindromicSubstring.php

--- LongestPalindromicSubstring.php
+++ LongestPalindromicSubstring.php
@@ -46,3 +46,57 @@ class Solution {
     return $maxStr;
    }
 }
+                                                          
+ 我的实现。写的太烂了                                                         
+                                                          
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     * 只能奇数对称。偶数对称还没有出来 
+     */
+    function lengthOfLongestSubstring($s) {
+        $length = strlen($s);
+        $key = 0;
+        $max = 0;
+        for($i = 0; $i < $length; $i++) {
+            $tmp = 0;
+            for($j=1; ($i + $j < $length) && ($i - $j >= 0);$j++) 
+            {
+               echo "i：".$i."j：".$j."tmp:".$key."\n";
+               
+            //   ji'shu奇数 
+               if($s[$i + $j] != $s[$i - $j]) {
+                   $key = $tmp > $max ? $i : $key;
+                   $max = $tmp > $max ? $i : $max;
+                   break;
+               }
+               
+            //   偶数
+            //  if($s[$i + $j - 1 ] != $s[ $i - $j ]) {
+            //       $key = $tmp > $max ? $i : $key;
+            //       $max = $tmp > $max ? $i : $max;
+            //       break;
+            //   }
+
+                $tmp++;
+                if($tmp > $max)
+                {
+                    $max = $tmp;
+                    $key = $i;
+                }
+            }
+        }
+        
+        $str = substr($s,$key - $max, 2*$max + 1);
+        return $str;
+        return $key ." ".$max;
+    }
+}
+
+$a = new Solution();
+echo $a->lengthOfLongestSubstring("fvffbffva");
+die();
+?>
